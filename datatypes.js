9fd4237860e4f2efd458f0379c5771e16e249f28
Refilwe15 <mrefilwe880@gmail.com>
2025-05-12 18:00:55 +0200

    updated

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+let integerNumber = 10;
+let floatingNumber = 3.14;
+
+let addition = integerNumber + floatingNumber;
+let subtraction = integerNumber - floatingNumber;
+let multiplication = integerNumber * floatingNumber;
+let division = integerNumber / floatingNumber;
+let modulus = integerNumber % 3;
+let exponentiation = integerNumber ** 2;
+
+console.log("Integer Number:", integerNumber);
+console.log("Floating Number:", floatingNumber);
+console.log("Addition:", addition);
+console.log("Subtraction:", subtraction);
+console.log("Multiplication:", multiplication);
+console.log("Division:", division);
+console.log("Modulus:", modulus);
+console.log("Exponentiation:", exponentiation);
