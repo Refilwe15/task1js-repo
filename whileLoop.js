19da3c81219c2784d45bf0f4fdaf4194ef221c06
Refilwe15 <mrefilwe880@gmail.com>
2025-05-12 18:06:28 +0200

    updated

---
+++ whileLoop.js
@@ -0,0 +1,29 @@
+let i = 1;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
+let j = 1;
+while (j <= 20) {
+    if (j % 2 === 0) {
+        console.log(j);
+    }
+    j++;
+}
+
+let k = 1;
+let sum = 0;
+while (k <= 100) {
+    sum += k;
+    k++;
+}
+console.log("Sum of numbers from 1 to 100:", sum);
+
+let m = 1;
+while (m < 50) {
+    if (m % 5 === 0) {
+        console.log(m);
+    }
+    m++;
+}
